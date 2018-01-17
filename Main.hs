de01002d69ea2950e454604ffbe46409bf876a76
MarijaZivkovic <marijaz93@yahoo.com>
2018-01-17 23:08:01 +0100

    Dodato stanje, funkcija play i obrada klika

--- Main.hs
+++ Main.hs
@@ -3,30 +3,63 @@ module Main(main) where
 import Graphics.Gloss
 import System.Random
 import Data.List
+import Graphics.Gloss.Interface.Pure.Game
+import Data.Fixed
 
+data Board = Board
+  { clicked :: [Int]
+  , unclicked :: [Int]
+}
 
+-- pocetno stanje table - nijedno polje nije kliknuto
+initialState :: Board
+initialState = Board [] [0..80]
+
+-- funkcija za iscrtavanje table - trebalo bi proci kroz listu "clicked" i iscrtati odgovarajuce slike u zavisnosti od numbers
+drawBoard :: Board -> Picture
+drawBoard board = Pictures $ grid : tiles where
+   tiles = map (drawTile) $ clicked board
+   grid = pictures [translate i j $ color blue $ rectangleWire 50 50 | i <- [-200,-150..200], j <- [-200,-150..200]]
+
+drawTile :: Int -> Picture
+drawTile index = pictures [translate (-200.0 + 50.0 * fromIntegral(index - (index `div` 9)*9)) (-200.0 + 50.0 * fromIntegral(index `div` 9)) $ color red $ rectangleSolid 50 50]
+  
+-- funkcija koja na osnovu koordinata racuna odgovarajuci indeks u listi polja
+coordinatesToIndex :: Float -> Float -> Int
+coordinatesToIndex x y = let k = (x+200) `div'` 50
+			     l = (y+200) `div'` 50
+			 in l*9+k
+  
+-- funkcija za obradu levog klika misa
+handleKeys :: Event -> Board -> Board
+handleKeys (EventKey (MouseButton LeftButton) Down _ (x,y)) board = changeState (coordinatesToIndex x y) board
+handleKeys _ board = board
+
+-- promena stanja kada dodje do klika
+changeState :: Int -> Board -> Board
+changeState tile board = board { clicked = tile : clicked board, unclicked = delete tile $ unclicked board}
+  
 window :: Display
 window = InWindow "Minesweeper" (450, 450) (10, 10)
+
+fps :: Int
+fps = 60
 
 background :: Color
 background = light (light blue)
 
--- crtanje mreze
-drawing :: Picture
-drawing = pictures
-    [translate i j $ color gridColor $ rectangleWire 50 50 | i <- [-200,-150..200], j <- [-200,-150..200]]
-  where gridColor = blue
-
 main :: IO ()
 main = do
   g <- newStdGen
+  one <- loadBMP "1.bmp"
   print "Indeksi polja gde su mine:"
   print $ mineTiles 0 81 g
   print "Matrica:"
   print $ numbers g
-  --one <- loadBMP "1.bmp"
-  display window background drawing
-
+  --print $ coordinatesToIndex (2.00) (2.00)
+  play window background fps initialState drawBoard handleKeys (flip const)
+  
+  
 -- generisanje 10 nasumicnih brojeva (indeksa gde ce biti smestene mine)
 mineTiles :: RandomGen g => Int -> Int -> g -> [Int]
 mineTiles a b g = take 10 $ nub (randomRs (a, b) g)
@@ -51,8 +84,8 @@ adjacentTilesIndices index
   | otherwise = [index - 10, index - 9, index - 8, index - 1, index + 1, index + 8, index + 9, index + 10]
 
 -- lista indeksa susednih poljima gde su mine
-fullMatrix :: RandomGen g => g -> [Int]
-fullMatrix g = concat (map (adjacentTilesIndices) (mineTiles 0 81 g))
+indicesAdjacentToMines :: RandomGen g => g -> [Int]
+indicesAdjacentToMines g = concat (map (adjacentTilesIndices) (mineTiles 0 81 g))
 
 -- broj pojavljivanja elementa u listi
 count :: Eq a => a -> [a] -> Int
@@ -60,7 +93,7 @@ count x xs = foldl (\acc y -> if x == y then acc+1 else acc) 0 xs
 
 -- lista sa rasporedjenim odgovarajucim brojevima i brojem 9 na mestima gde su mine
 numbers :: RandomGen g => g -> [(Int, Int)]
-numbers g = sort ((zip ([0..] \\ (mineTiles 0 81 g)) [count x (fullMatrix g) | x <- ([0..80] \\ (mineTiles 0 81 g))]) `union` (zip (mineTiles 0 81 g) [9,9..]))
+numbers g = sort ((zip ([0..] \\ (mineTiles 0 81 g)) [count x (indicesAdjacentToMines g) | x <- ([0..80] \\ (mineTiles 0 81 g))]) `union` (zip (mineTiles 0 81 g) [9,9..]))
 
 
 -- 0  1  2  3  4  5  6  7  8
